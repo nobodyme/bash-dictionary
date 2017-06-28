a41358ffc89eb6365e3cb7442053fe0f4f34e777
Nobody <nobodydisturbme@gmail.com>
2017-06-28 17:08:58 +0530

    Dictionary added to terminal

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo -n "Installing dictionary: "
+chmod a+x meaning
+cp meaning /usr/local/bin > /dev/null 2>&1 || { echo "Failure"; echo "Error copying file, try running install script as sudo"; exit 1; }
+echo "Success"
+
